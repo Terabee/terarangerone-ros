0f7885b95124d3108f84d7234848845aeb47a580
Flavio Fontana <fly.fontana@gmail.com>
2014-10-16 13:54:11 +0200

    updated the format to comply with the ros style guide

--- src/terarangerone.cpp
+++ src/terarangerone.cpp
@@ -1,50 +1,51 @@
 /****************************************************************************
-*
-* Copyright (C) 2014 Flavio Fontana & Luis Rodrigues. All rights reserved.
-* Author: Flavio Fontana <fly.fontana@gmail.com>
-* Author: Luis Rodrigues <luis.rodrigues@terabee.com>
+ *
+ * Copyright (C) 2014 Flavio Fontana & Luis Rodrigues. All rights reserved.
+ * Author: Flavio Fontana <fly.fontana@gmail.com>
+ * Author: Luis Rodrigues <luis.rodrigues@terabee.com>
 
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-*
-* 1. Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* 2. Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in
-* the documentation and/or other materials provided with the
-* distribution.
-* 3. Neither the name TerarangerOne nor the names of its contributors may be
-* used to endorse or promote products derived from this software
-* without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*
-****************************************************************************/
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ * 3. Neither the name TerarangerOne nor the names of its contributors may be
+ * used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
 
 #include <string>
 
 #include "terarangerone/terarangerone.h"
 
-namespace terarangerone {
+namespace terarangerone
+{
 
-TerarangerOne::TerarangerOne() {
+TerarangerOne::TerarangerOne()
+{
   // Get paramters
   ros::NodeHandle private_node_handle_("~");
-  private_node_handle_.param("portname", port_name_,
-    std::string("/dev/ttyUSB0"));
+  private_node_handle_.param("portname", port_name_, std::string("/dev/ttyUSB0"));
 
   // Publishers
   range_publisher_ = nh_.advertise<sensor_msgs::Range>("terarangerone", 1);
@@ -57,14 +58,14 @@ TerarangerOne::TerarangerOne() {
   serial_port_->setSerialCallbackFunction(&serial_data_callback_function_);
 
   // Connect serial port
-  if ( !serial_port_->connect(port_name_) ) {
+  if (!serial_port_->connect(port_name_))
+  {
     ros::shutdown();
     return;
   }
 
   // Output loaded parameters to console for double checking
-  ROS_INFO("%s is up and running with the following parameters:",
-    ros::this_node::getName().c_str());
+  ROS_INFO("%s is up and running with the following parameters:", ros::this_node::getName().c_str());
   ROS_INFO("port_name: %s", port_name_.c_str());
 
   // Set operation Mode
@@ -75,21 +76,25 @@ TerarangerOne::TerarangerOne() {
   dyn_param_server_.setCallback(dyn_param_server_callback_function_);
 }
 
-TerarangerOne::~TerarangerOne() {
+TerarangerOne::~TerarangerOne()
+{
 }
 
-uint8_t TerarangerOne::crc8(uint8_t *p, uint8_t len) {
+uint8_t TerarangerOne::crc8(uint8_t *p, uint8_t len)
+{
   uint16_t i;
   uint16_t crc = 0x0;
 
-  while (len--) {
+  while (len--)
+  {
     i = (crc ^ *p++) & 0xFF;
     crc = (crc_table[i] ^ (crc << 8)) & 0xFF;
   }
   return crc & 0xFF;
 }
 
-void TerarangerOne::serialDataCallback(uint8_t single_character) {
+void TerarangerOne::serialDataCallback(uint8_t single_character)
+{
   static uint8_t input_buffer[BUFFER_SIZE];
   static int buffer_ctr = 0;
   static int seq_ctr = 0;
@@ -100,21 +105,26 @@ void TerarangerOne::serialDataCallback(uint8_t single_character) {
   range_msg.min_range = 0.2;
   range_msg.radiation_type = sensor_msgs::Range::INFRARED;
 
-  if ( single_character != 'T' && buffer_ctr < 4 ) {
+  if (single_character != 'T' && buffer_ctr < 4)
+  {
     // not begin of serial feed so add char to buffer
-    input_buffer[ buffer_ctr++ ] = single_character;
-  } else if ( single_character == 'T' && buffer_ctr == 4 ) {
+    input_buffer[buffer_ctr++] = single_character;
+  }
+  else if (single_character == 'T' && buffer_ctr == 4)
+  {
     // end of feed, calculate
     int16_t crc = crc8(input_buffer, 3);
 
-    if ( crc == input_buffer[3] ) {
+    if (crc == input_buffer[3])
+    {
       int16_t range = input_buffer[1] << 8;
       range |= input_buffer[2];
 
-      if ( range < 14000 && range > 200 ) {
+      if (range < 14000 && range > 200)
+      {
         range_msg.header.stamp = ros::Time::now();
         range_msg.header.seq = seq_ctr++;
-        range_msg.range = range * 0.001;  // convert to m
+        range_msg.range = range * 0.001; // convert to m
         range_publisher_.publish(range_msg);
       }
     }
@@ -124,40 +134,48 @@ void TerarangerOne::serialDataCallback(uint8_t single_character) {
     // clear struct
     bzero(&input_buffer, BUFFER_SIZE);
     // store T
-    input_buffer[ buffer_ctr++ ] = single_character;
+    input_buffer[buffer_ctr++] = single_character;
 
-  } else if ( buffer_ctr >= 4 ) {
+  }
+  else if (buffer_ctr >= 4)
+  {
     buffer_ctr = 0;
   }
 }
 
-void TerarangerOne::setMode(char c) {
+void TerarangerOne::setMode(char c)
+{
   printf("Sending Mode command %c\n", c);
   serial_port_->sendChar(c);
 }
 
-void TerarangerOne::dynParamCallback(const terarangerone::TerarangerOneConfig &config,
-                                     uint32_t level) {
-  if ( config.Speed == terarangerone::TerarangerOne_Fast ) {
+void TerarangerOne::dynParamCallback(const terarangerone::TerarangerOneConfig &config, uint32_t level)
+{
+  if (config.Speed == terarangerone::TerarangerOne_Fast)
+  {
     setMode(FAST_MODE);
   }
 
-  if ( config.Speed == terarangerone::TerarangerOne_Precise ) {
+  if (config.Speed == terarangerone::TerarangerOne_Precise)
+  {
     setMode(PRECISE_MODE);
   }
 
-  if ( config.Environment == terarangerone::TerarangerOne_Indoor ) {
+  if (config.Environment == terarangerone::TerarangerOne_Indoor)
+  {
     setMode(INDOOR_MODE);
   }
 
-  if ( config.Environment == terarangerone::TerarangerOne_Outdoor ) {
+  if (config.Environment == terarangerone::TerarangerOne_Outdoor)
+  {
     setMode(OUTDOOR_MODE);
   }
 }
 
-}  // namespace terarangerone
+} // namespace terarangerone
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "terarangerone");
   terarangerone::TerarangerOne tera_bee;
   ros::spin();
